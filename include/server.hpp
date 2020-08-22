4c80fbd3ef049bca6a34830345f19e03d51463cc
David Fan <fand.kanade@gmail.com>
2020-08-22 11:28:45 -0400

    Code cleanup

--- include/server.hpp
+++ include/server.hpp
@@ -40,24 +40,12 @@ public:
     esp_err_t unregisterAllUriHandles(void);
     esp_err_t unregisterAllUriHandles(char* uri);
 
-    // esp_http_client_handle_t getClient(void) {
-    //     return this->client;
-    // }
-    // esp_err_t postJSON(cJSON *message, size_t size=0);
-    // esp_err_t postFormattedJSON(char *message);
-    // esp_err_t deinit(void) {
-    //     return esp_http_client_cleanup(this->client);
-    // }
-
 private:
     httpd_handle_t server;
     httpd_config_t config;
     rest_server_context_t *context;
 
     std::vector<httpd_uri_t*> registered_uri;
-    // char response_buffer[MAX_HTTP_OUTPUT_BUFFER];
-    // esp_http_client_config_t config;
-    // esp_http_client_handle_t client;
 };
 }
 #endif
