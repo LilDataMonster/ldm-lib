9a620e0b5d4f1f68fd51e9626e489e07caa940c3
David Fan <fand.kanade@gmail.com>
2020-10-03 21:09:46 -0400

    Added blufi gap defaults logging

--- src/blufi_defaults.cpp
+++ src/blufi_defaults.cpp
@@ -91,6 +91,30 @@ void LDM::BLE::defaultGapHandler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_pa
         esp_ble_gap_start_advertising(&LDM::BLE::default_blufi_adv_params);
         ESP_LOGI(TAG, "Started GAP Advertising");
         break;
+    case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+        /* advertising start complete event to indicate advertising start successfully or failed */
+        if(param->adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(TAG, "Advertising start failed");
+        } else {
+            ESP_LOGI(TAG, "Advertising start successfully");
+        }
+        break;
+    case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
+        if(param->adv_stop_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(TAG, "Advertising stop failed");
+        } else {
+            ESP_LOGI(TAG, "Stop advertising successfully\n");
+        }
+        break;
+    case ESP_GAP_BLE_UPDATE_CONN_PARAMS_EVT:
+        ESP_LOGI(TAG, "Update connection params status = %d, min_int = %d, max_int = %d,conn_int = %d,latency = %d, timeout = %d",
+              param->update_conn_params.status,
+              param->update_conn_params.min_int,
+              param->update_conn_params.max_int,
+              param->update_conn_params.conn_int,
+              param->update_conn_params.latency,
+              param->update_conn_params.timeout);
+        break;
     default:
         break;
     }
